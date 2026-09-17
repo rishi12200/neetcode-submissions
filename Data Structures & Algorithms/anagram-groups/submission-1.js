4e9081e33fb62f1da4d365405279fcb3176b37a5
Rishi <48964307+rishi12200@users.noreply.github.com>
2026-09-17 17:43:30 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = {};
+
+        for(let s of strs) {
+            const key = s.split('').sort().join('');
+
+            if(!map[key]) {
+                map[key] = []
+            }
+            map[key].push(s)
+        }
+        return Object.values(map)
+    }
+}
